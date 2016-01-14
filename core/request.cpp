c3c056be829b343cebb3f9044d43d09839e975b1
Jon0 <Jono4728@gmail.com>
2016-01-14 16:32:29 +1300

    state machines

--- core/request.cpp
+++ core/request.cpp
@@ -2,6 +2,22 @@
 
 namespace core {
 
+
+function::function() {
+
+}
+
+
+int function::compute() {
+    current_state = &states.at(0);
+    while (!current_state->write) {
+        int read = input->compute();
+        current_state = &states.at(read);
+    }
+    return current_state->write;
+}
+
+
 request::request() {
 
 }
--- core/request.h
+++ core/request.h
@@ -2,9 +2,39 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace core {
+
+/**
+ * function internal state
+ */
+struct state {
+    int write;
+    std::unordered_map<int, int> transitions;
+};
+
+
+/**
+ * functions used for transforming input streams
+ */
+class function {
+public:
+    function();
+
+    int compute();
+
+private:
+
+    // there could be many inputs
+    function *input;
+
+    state *current_state;
+
+    std::unordered_map<int, state> states;
+};
+
 
 class request {
 public:
